7bb3e1095149a2eaf71b67f75a78db4e01923437
Wojciech Smolarek <kontakt@wojiechsmolarek.pl>
2025-04-08 21:24:06 +0200

    Basic config

--- .netlify/functions-internal/chunks/about_e242606c.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/about_ec27b0be.mjs').then(n => n.a);
-
-export { page };
--- .netlify/functions-internal/chunks/contact_0d81bc09.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/contact_6d05ef13.mjs').then(n => n.c);
-
-export { page };
