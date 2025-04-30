e09144a1423b4e607f56b626c318c15c60153eff
Wojciech Smolarek <wojteksmolarek7@gmail.com>
2025-04-30 11:07:13 +0200

    Update font tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
       },
       fontFamily: {
         dm: ["DM Serif Display", "serif"],
-        jost: ["Inter", "sans-serif"], // Ensure fonts with spaces have " " surrounding it.
+        jost: ["Geist Sans", "sans-serif"], // Ensure fonts with spaces have " " surrounding it.
       },
       colors: {
         // Configure your color palette here
