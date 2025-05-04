ca61a33d619114f22ca1df38f33a5cd030f8b57a
Wojciech Smolarek <kontakt@wojiechsmolarek.pl>
2025-05-04 20:43:00 +0200

    chore: update dependencies & change logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// import vercelServerless from "@astrojs/vercel/serverless";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel(),
+  devToolbar: {
+    enabled: false
+  },
 });
