e5723ceb476ee25dcabb8e2b1d62401e314cd1aa
Wojciech Smolarek <wojteksmolarek7@gmail.com>
2025-05-14 11:33:19 +0200

    CSS changes on contatct page

--- src/components/contact-box.tsx
+++ src/components/contact-box.tsx
@@ -5,25 +5,11 @@ const ContactInfo = () => {
   return (
     <div className="card card-lg w-full max-w-[480px] mx-auto bg-primary-300 rounded-[36px] shadow-lg px-8 py-12 flex flex-col gap-8">
       <div className="card-header mb-2">
-        <h2 className="text-[28px] lg:text-[36px] font-dm text-primary-200 text-center tracking-wide leading-tight">
+        <h2 className="text-[28px] font-dm text-primary-200 text-center tracking-wide leading-tight">
           Dane kontaktowe
         </h2>
       </div>
       <div className="card-body flex flex-col gap-7">
-        <a
-          href="mailto:kontakt@wusp-jarocin.pl"
-          className="flex items-center gap-4 text-xl font-jost tracking-tight text-text-gray-200 hover:text-primary-200 transition-colors duration-200 justify-center"
-        >
-          <Mail className="w-7 h-7 text-primary-200" />
-          kontakt@wusp-jarocin.pl
-        </a>
-        <a
-          href="tel:+48607442592"
-          className="flex items-center gap-4 text-xl font-jost tracking-tight text-text-gray-200 hover:text-primary-200 transition-colors duration-200 justify-center"
-        >
-          <Phone className="w-7 h-7 text-primary-200" />
-          +48 607 442 592
-        </a>
         <div className="flex flex-col gap-1 text-center text-lg font-jost tracking-tight text-text-gray-200">
           <span className="font-semibold text-primary-200">
             Wytwórczo Usługowa Spółdzielnia Pracy Jarocin
@@ -33,6 +19,20 @@ const ContactInfo = () => {
           <span>KRS: 0000146398</span>
           <span>REGON: 000843052</span>
         </div>
+        <a
+          href="mailto:kontakt@wusp-jarocin.pl"
+          className="flex items-center gap-4 text-lg font-jost tracking-tight text-text-gray-200 hover:text-primary-200 transition-colors duration-200 justify-center"
+        >
+          <Mail className="w-7 h-7 text-primary-200" />
+          kontakt@wusp-jarocin.pl
+        </a>
+        <a
+          href="tel:+48607442592"
+          className="flex items-center gap-4 text-lg font-jost tracking-tight text-text-gray-200 hover:text-primary-200 transition-colors duration-200 justify-center"
+        >
+          <Phone className="w-7 h-7 text-primary-200" />
+          +48 607 442 592
+        </a>
       </div>
     </div>
   );
