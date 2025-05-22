b2ed000f5644c3ab90fb444ba0e265f995a549e7
Wojciech Smolarek <wojteksmolarek7@gmail.com>
2025-05-22 13:34:22 +0200

    update: queote h4 to span

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -14,9 +14,9 @@ const QuoteText = ({ text, name }: QuoteProps) => {
       <LeftPattern />
       <div className="flex flex-col w-full items-center justify-center gap-4 lg:gap-8 lg:mx-[-30px]">
         <Quote />
-        <h4 className="text-[20px] leading-[33px] lg:text-[35px] lg:leading-[43.75px] italic tracking-wide font-dm text-primary-200 text-center">
+        <span className="text-[20px] leading-[33px] lg:text-[35px] lg:leading-[43.75px] italic tracking-wide font-dm text-primary-200 text-center">
           {text}
-        </h4>
+        </span>
         <p className="text-text-gray text-base tracking-tight lg:text-[25px] font-jost lg:leading-[37px] text-center ">
           {" "}
           {`-${name}`}{" "}
