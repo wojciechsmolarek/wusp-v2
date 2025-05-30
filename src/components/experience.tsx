fe171dce5058a0e891a6401306ce72573ff98dc7
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2025-05-30 12:35:12 +0200

    Changes in gellery and offer page

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,5 +1,3 @@
-import CountUp from "react-countup";
-
 const Experience = ({
   text = "Default Text",
   num,
@@ -13,9 +11,9 @@ const Experience = ({
     <div
       className={`flex min-w-[243px] flex-col ${
         i === 3 ? "" : "md:border-r"
-      } border-primary-100 items-center justify-center gap-4]`}
+      } border-primary-500 items-center justify-center gap-4]`}
     >
-      <span className="text-primary-100 text-[50px] lg:text-[85px] tracking-widest leading-[70px] font-dm lg:leading-[106.25px] ">
+      <span className="text-primary-500 text-[50px] lg:text-[85px] tracking-widest leading-[70px] font-dm lg:leading-[106.25px] ">
         {num}
       </span>
       <p className="text-text-gray-200 text-base text-center lg:text-lg font-jost tracking-tight lg:leading-9">
