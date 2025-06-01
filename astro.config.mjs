75c1f101be0d10b156067e1a2652eced1c1ef606
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2025-06-01 12:12:50 +0200

    Update GA script to collect data v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 import jopSoftwarecookieconsent from "@jop-software/astro-cookieconsent";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: 'https://wusp-jarocin.pl/',
@@ -12,6 +13,12 @@ export default defineConfig({
     tailwind(),
     react(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+        loadScriptsOnMainThread: ["gtag", "dataLayer"]
+      }
+    }),
     jopSoftwarecookieconsent({
       guiOptions: {
         consentModal: {
